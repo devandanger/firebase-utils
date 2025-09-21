18713a098dfab95f38c98de65a3f303e55cc25f1
Evan Anger <devandanger@gmail.com>
2025-09-21 06:37:48 -0400

    refactor: rename package to @firebase-toolbox/fsdiff and update dependencies
    - Changed package name from @firebase-utils/fsdiff to @firebase-toolbox/fsdiff in package.json.
    - Rearranged dependencies in package.json for better organization.
    - Added eslint and prettier to devDependencies in package.json.
    - Created a new ESLint configuration file (eslint.config.js) with custom rules and settings.

---
+++ eslint.config.js
@@ -0,0 +1,40 @@
+export default [
+  {
+    files: ['**/*.{js,mjs}'],
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: 'module',
+      globals: {
+        console: 'readonly',
+        process: 'readonly',
+        Buffer: 'readonly',
+        __dirname: 'readonly',
+        __filename: 'readonly',
+        global: 'readonly',
+        setTimeout: 'readonly',
+        clearTimeout: 'readonly',
+        setInterval: 'readonly',
+        clearInterval: 'readonly'
+      }
+    },
+    rules: {
+      'no-unused-vars': 'warn',
+      'no-console': 'off',
+      'semi': ['error', 'always'],
+      'quotes': ['error', 'single']
+    }
+  },
+  {
+    files: ['test/**/*.{js,mjs}'],
+    languageOptions: {
+      globals: {
+        describe: 'readonly',
+        it: 'readonly',
+        before: 'readonly',
+        after: 'readonly',
+        beforeEach: 'readonly',
+        afterEach: 'readonly'
+      }
+    }
+  }
+];
